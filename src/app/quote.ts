72ad7a7589fdd71ad234fe975d7e5a50cb01538d
Olliemint <furywrld5@gmail.com>
2022-04-09 20:05:02 +0300

    creating quote component and class quote

---
+++ src/app/quote.ts
@@ -0,0 +1,7 @@
+export class Quote {
+    seeMore= boolean;
+    constructor(public Qoutetitle:string,public Quote:string,public Author:string,public upVotes:number,public downVotes:number,public submittedBy:string){
+        this.seeMore = false;
+
+    }
+}
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  header= 'Quote-Medium'
+
+  Quotes: Quote[] =[
+    new Quote('Inspirational','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela',0,0,'Trevor K.'),
+    new Quote('Motivational','We cannot solve problems with the kind of thinking we employed when we came up with them.','Albert Einstein',0,0,'Collins Leteipa'),
+    new Quote('Nature','I go to nature to be soothed and healed, and to have my senses put in order.','John Burroughs',0,0,'Terry Wambui'),
+    new Quote('Life','Let the rain kiss you. Let the rain beat upon your head with silver liquid drops. Let the rain sing you a lullaby.','Langston Hughes',0,0,'Eve Bet'),
+    new Quote('')
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
