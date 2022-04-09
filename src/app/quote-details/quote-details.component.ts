39eaf0b57ea578a3e8ee139c3740246e4624244a
Olliemint <furywrld5@gmail.com>
2022-04-09 21:58:53 +0300

    generated quote details component and bind with parent component

---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+// import { Quote } from '../quote';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+
+  @Input() Quotes!: Quote;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
